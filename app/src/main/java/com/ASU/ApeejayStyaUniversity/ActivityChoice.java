34007a17843622390764f0b9ad041ebb18ef89f4
Ashish1414 <ashubansal.ashishbansal@gmail.com>
2019-09-08 02:27:22 +0530

    moodle and About ASU done

--- app/src/main/java/com/ASU/ApeejayStyaUniversity/ActivityChoice.java
+++ app/src/main/java/com/ASU/ApeejayStyaUniversity/ActivityChoice.java
@@ -1,10 +1,12 @@
 package com.ASU.ApeejayStyaUniversity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +29,22 @@ public class ActivityChoice extends AppCompatActivity {
                 editor.commit();
                 onBackPressed();
 
+            }
+        });
+        findViewById(R.id.abt_apeejay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityChoice.this,AboutASU.class);
+                startActivityForResult(intent, 0);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+        findViewById(R.id.Moodle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://ecourse.asu.apeejay.edu/login/index.php"));
+                startActivity(intent);
             }
         });
     }
