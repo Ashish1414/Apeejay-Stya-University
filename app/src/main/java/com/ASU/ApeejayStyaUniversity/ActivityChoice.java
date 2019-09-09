6c0c685ee5319577ba9dc84b925be6d406bd5e91
Ashish1414 <ashubansal.ashishbansal@gmail.com>
2019-09-09 14:53:44 +0530

    updated!

--- app/src/main/java/com/ASU/ApeejayStyaUniversity/ActivityChoice.java
+++ app/src/main/java/com/ASU/ApeejayStyaUniversity/ActivityChoice.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,22 @@ public class ActivityChoice extends AppCompatActivity {
         setContentView(R.layout.activity_choice);
         sharedPreferences = getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
         btnLogOut = findViewById(R.id.btnLogOut);
+        findViewById(R.id.abt_apeejay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityChoice.this,AboutASU.class);
+                startActivityForResult(intent, 0);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+        findViewById(R.id.Moodle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://ecourse.asu.apeejay.edu/login/index.php"));
+                startActivity(intent);
+            }
+        });
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,7 +46,15 @@ public class ActivityChoice extends AppCompatActivity {
 
             }
         });
+        findViewById(R.id.apricot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Apricot.class);
+                startActivity(intent);
+            }
+        });
     }
+
     public void onBackPressed() {
         Intent intent = new Intent(ActivityChoice.this,UserSelect.class);
         startActivityForResult(intent, 0);
