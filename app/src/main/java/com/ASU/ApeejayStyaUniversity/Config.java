e4cf32433a43f6f0fcd6b28780001c0d4261b309
Ashish1414 <ashubansal.ashishbansal@gmail.com>
2019-09-15 21:59:05 +0530

    sign in resolved

--- app/src/main/java/com/ASU/ApeejayStyaUniversity/Config.java
+++ app/src/main/java/com/ASU/ApeejayStyaUniversity/Config.java
@@ -2,5 +2,5 @@ package com.ASU.ApeejayStyaUniversity;
 
 public class Config {
     public static final String EMAIL ="ashishbansal869@gmail.com"; //your-gmail-username
-    public static final String PASSWORD ="111214ashu"; //your-gmail-password
+    public static final String PASSWORD ="wearefive5"; //your-gmail-password
 }
